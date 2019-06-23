d0782b612431e2b8a4393cd68d47c7bea907e2a6
unknown <ulyana250188@gmail.com>
2019-06-24 02:41:01 +0300

    setup react to git

---
+++ build/precache-manifest.2fcd558b22f12570694d4b68da13caac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57aefb59e1ee4596d06f25276c2de8ec",
+    "url": "/organisation_api/index.html"
+  },
+  {
+    "revision": "b98a67c6eee77bfc0bff",
+    "url": "/organisation_api/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4f8b3969e53073c68ad3",
+    "url": "/organisation_api/static/css/main.56850682.chunk.css"
+  },
+  {
+    "revision": "b98a67c6eee77bfc0bff",
+    "url": "/organisation_api/static/js/2.970cc216.chunk.js"
+  },
+  {
+    "revision": "4f8b3969e53073c68ad3",
+    "url": "/organisation_api/static/js/main.b6b78df5.chunk.js"
+  },
+  {
+    "revision": "3c74c4056e1e1338cc24",
+    "url": "/organisation_api/static/js/runtime~main.39cbe81f.js"
+  }
+]);
